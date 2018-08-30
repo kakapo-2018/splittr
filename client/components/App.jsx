0b2934c42fea3c4fd9c8294a600f6a04519bc279
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-08-31 10:12:38 +1200

    first circle working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,22 +2,48 @@ import React from 'react'
 
 import Circle from './Circle'
 
-const App = () => {
+class App extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {   
+      circle:[{
+      cx: Math.floor(Math.random()*window.innerWidth ),
+      cy: Math.floor(Math.random()*window.innerHeight),
+      level: 0,
+      r:Math.floor(Math.random()*256),
+      color: "red"
+    }, {
+      cx: Math.floor(Math.random()*window.innerWidth ),
+      cy: Math.floor(Math.random()*window.innerHeight),
+      level: 0,
+      r:Math.floor(Math.random()*256),
+      color: "red"
+    }]}
+
+    // this.handleClick = this.handleClick.bind(this, props.circle)
+  }
+
+  render(){
   const width = window.innerWidth
   const height = window.innerHeight
 
-  const circle = {
-    cx: Math.floor(Math.random()*width),
-    cy: Math.floor(Math.random()*height),
-    level: 0,
-    r: Math.floor(Math.random()*256)
-  }
+  console.log(this.state.circle[0].cx)
 
   return (
+
+  
+
     <svg width={width} height={height}>
-      <Circle circle={circle} />
+    <g>
+    <circle cx={this.state.circle[0].cx} cy={this.state.circle[0].cy} r={this.state.circle[0].r} fill={this.state.circle[0].color}/>
+    <circle cx={this.state.circle[1].cx} cy={this.state.circle[1].cy} r={this.state.circle[1].r} fill={this.state.circle[1].color}/>
+      {/* <Circle circle={this.state.circle} /> */}
+      </g>
     </svg>
   )
+}
 }
 
 export default App
